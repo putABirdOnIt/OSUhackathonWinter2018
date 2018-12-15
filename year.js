44647755164b525c46f98ab01af73659e13a13e6
ylijokic <44416709+ylijokic@users.noreply.github.com>
2018-12-14 20:54:35 -0800

    t

---
+++ year.js
@@ -0,0 +1,32 @@
+
+//Set yearInput varibale equal to id-selector = 'yearInput' from HTML file
+var yearInput = document.querySelector('#yearInput');
+
+
+// Declare another variable so that we can hold the value of our
+// input field. Assign with `null` at first so we will know it's empty
+var yearValue = null;
+
+// This is our function that we want to react to the input element's events
+function handleInput(event) {
+  //Is console.log() similar to cout in C++?
+
+  console.log(event.target.value);
+
+  // Check if the input's value has at least 4 digits
+  if (event.target.value / 1000 >= 1) {
+    if (event.target.value > 2000) {
+      // If number is above 2000, have the browser display an alert dialog to the user
+      window.alert('Please enter a year before 2000')
+    } else if (event.target.value < 1980) {
+      // If number is below 1980, have the browser display an alert dialog to the user
+      window.alert('Please enter a year after 1980')
+    } else {
+      // Set yearValue equal to'event' argument property 'target' which accesses 'value' currently held inside of input element  
+      // Not sure if this will work!
+      yearValue = event.target.value;
+    }
+  }
+}
+
+yearInput.addEventListener('input', handleInput)
