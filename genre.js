01a48d77503231186ec8ed11579719a92c8bc808
Calista Wong <wongcal@oregonstate.edu>
2018-12-15 15:38:00 -0800

    add session storage

--- genre.js
+++ genre.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 		e.preventDefault(); //so we don't submit the form again
 		var genreValue = null;
 		genreValue = document.getElementById("getGenre").elements[0].value; //grab genre value
-		//sessionStorage.setItem('genre', genreValue);
+		sessionStorage.setItem('genre', genreValue);
 		window.location.href = 'console.html'; //redirect to next page
 	});
 
