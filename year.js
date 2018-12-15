5107634e0fe1ddb007a9a4fbfa6b2ece1157b313
Calista Wong <wongcal@oregonstate.edu>
2018-12-14 23:04:47 -0800

    retrieve yearInput variable and edit error functions for window alerts

--- year.js
+++ year.js
@@ -4,38 +4,38 @@ $(document).ready(function () {
     $('#getYear').submit(function (e){
             	e.preventDefault(); //so we don't submit the form again
 
+		// Declare another variable so that we can hold the value of our
+		// input field. Assign with `null` at first so we will know it's empty
+		var yearValue = null;
+	    	var x;
+
 	    	//Set yearInput variable equal to id-selector = 'yearInput' from HTML file
-		var yearInput = document.getElementById("getYear").elements[0].value; //we are accessing the first (and only) form
-		console.log(yearInput);
+		var x = document.getElementById("getYear").elements[0].value; //we are accessing the first (and only) form
+		
+	    	// Check if the input's value has at least 4 digits
+  		if (x / 1000 >= 1) 
+	    	{
+    			if (x > 2000) 
+			{
+      				// If number is above 2000, have the browser display an alert dialog to the user
+      				window.alert('Please enter a year before 2000');
+    			} 
+			else if (x < 1980) 
+			{
+      				// If number is below 1980, have the browser display an alert dialog to the user
+      				window.alert('Please enter a year after 1980');
+    			} 
+			else //year is good
+			{
+				yearValue = x; //assign to yearValue variable
+			}
+    		}
+	    	else //input value has less than 3 digits 
+	    	{
+			window.alert('Input must have 4 digits or more');	
+	    	}
+	    
     });
 });
 
-/*
-// Declare another variable so that we can hold the value of our
-// input field. Assign with `null` at first so we will know it's empty
-var yearValue = null;
 
-// This is our function that we want to react to the input element's events
-function handleInput(event) {
-  //Is console.log() similar to cout in C++?
-
-  console.log(event.target.value);
-
-  // Check if the input's value has at least 4 digits
-  if (event.target.value / 1000 >= 1) {
-    if (event.target.value > 2000) {
-      // If number is above 2000, have the browser display an alert dialog to the user
-      window.alert('Please enter a year before 2000')
-    } else if (event.target.value < 1980) {
-      // If number is below 1980, have the browser display an alert dialog to the user
-      window.alert('Please enter a year after 1980')
-    } else {
-      // Set yearValue equal to'event' argument property 'target' which accesses 'value' currently held inside of input element  
-      // Not sure if this will work!
-      yearValue = event.target.value;
-    }
-  }
-}
-
-yearInput.addEventListener('input', handleInput)
-*/
