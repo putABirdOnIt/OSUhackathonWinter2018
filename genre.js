5a23cec2c3b686ee02a5038c791d189652102eeb
Calista Wong <wongcal@oregonstate.edu>
2018-12-15 15:02:47 -0800

    add genre value to storage

--- genre.js
+++ genre.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 	
-	//
+	//grabbing user input from form
+	$('#getGenre').submit(function(e){
+		e.preventDefault(); //so we don't submit the form again
+		var genreValue = null;
+		genreValue = document.getElementById("getGenre").elements[0].value; //grab genre value
+		//sessionStorage.setItem('genre', genreValue);
+		window.location.href = 'platform.html'; //redirect to next page
+	});
 
 });
