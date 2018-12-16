fd181081bfa889d51c9aef8abb16bef46e610bce
Calista Wong <wongcal@oregonstate.edu>
2018-12-16 12:04:40 -0800

    API calls for getting genre and console

--- apiCall.js
+++ apiCall.js
@@ -1,24 +1,29 @@
-//$(document).ready(function(){
-	//grab query values from session storage
-	//var year = sessionStorage.year;
-	var year = '1990'; //for testing purposes
+/* Instructions: on command line/putty, run node apiCall.js
+ * There will be alot of results at one time so I would mute one section at a time to work on it */
+ 
 
-	//API call
-	var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-	var apiKey = 'db8cb3d339c6f4785beee44b79b3ec96';
-	var req = new XMLHttpRequest();
-	var queryString = 'https://api-endpoint.igdb.com/games/?search='+year+'&fields=name,first_release_date,human';
-	//var queryString = 'https://api-endpoint.igdb.com/release_dates/?search=65585&fields=id,game,y';
+ /* For this first call, you will see the resulting array results for platform_id and platform_names */ 
 
-	//queryString+=year;
-	req.open('GET', queryString, false);
-	req.withCredentials = true;
-	req.setRequestHeader("user-key", apiKey);
-	req.setRequestHeader("Accept", "application/json");
-	req.send(); //request json data
-	var response = JSON.parse(req.responseText); //response variable holds all the API data returned
-	console.log(response);
-	//console.log(response[3].first_release_date.human);
+/*
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var queryString = 'https://api.mobygames.com/v1/platforms?&api_key=FhVfEFsB6RGpjwCqLtrw1g==';
+var req = new XMLHttpRequest();
+req.open('GET', queryString, false);
+req.withCredentials = false;
+req.send(); //request json data
+var response = JSON.parse(req.responseText); //response variable holds all the API data returned
+console.log(response);
+*/
+
+/* For this second call, you will see the results for genre_id and genre_names */
+
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var queryString = 'https://api.mobygames.com/v1/genres?&api_key=FhVfEFsB6RGpjwCqLtrw1g==';
+var req = new XMLHttpRequest();
+req.open('GET', queryString, false);
+req.withCredentials = false;
+req.send(); //request json data
+var response = JSON.parse(req.responseText); //response variable holds all the API data returned
+console.log(response);
 
 
-//});
