9a5547d3f7623b3e0666b431869dbef81fa22ec0
Calista Wong <wongcal@oregonstate.edu>
2018-12-16 01:15:59 -0800

    added js and css code for results page

---
+++ js/results.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    //grab stored values
+    var year = sessionStorage.year;
+    var genre = sessionStorage.genre;
+    var platform = sessionStorage.console;
+    console.log("year: " + year);
+    console.log("genre: " + genre);
+    console.log("console: " + platform);
+});
