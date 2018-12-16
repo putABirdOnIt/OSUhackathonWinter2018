9f08948ce65aa9762acfc034daa878b747196669
Calista Wong <wongcal@oregonstate.edu>
2018-12-16 00:45:43 -0800

    made small change to syntax error

--- console.js
+++ console.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
 	e.preventDefault(); //so we don't submit the form again
 		var consoleValue = null;
 		consoleValue = document.getElementById("getConsole").elements[0].value; //grab console value
-		console.log("console: " + consoleValue);
 		sessionStorage.setItem('console', consoleValue);	
 		window.location.href = 'results.html'; //redirect to result page
 	});
